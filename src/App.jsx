ee205946a382da9e882ba6a7269a9c78b5cb2d0f
mbranches <marcusvbranches@gmail.com>
2025-06-19 10:56:47 -0300

    feat: adiciona app

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Calculator, FileText, Users } from "lucide-react";
+import CardMenu from "./components/CardMenu";
+import SideBar from "./components/SideBar";
+
+function App() {
+
+  return (
+    <div className="bg-slate-200">
+      <SideBar/>
+      <div className="min-h-screen min-w-scree flex justify-center items-center">
+
+        <div className="max-w-[1000px] flex items-center justify-center gap-4 flex-wrap">
+          <CardMenu icon={<Calculator size={24}/>} title="Criar Orçamento" description="Novo orçamento de obra" color="green"/>
+
+          <CardMenu icon={<FileText size={24}/>} title="Visualizar Orçamentos" description="Gerenciar orçamentos existentes" color="blue"/>
+          
+          <CardMenu icon={<Users size={24}/>} title="Visualizar Clientes" description="Novo orçamento de obra" color="purple"/>
+          
+          <CardMenu icon={<Calculator size={24}/>} title="Criar Cliente" description="Cadastrar novo cliente" color="orange"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
