28a56e817460601bf8de47643eda0f5e69a8bafa
mbranches <marcusvbranches@gmail.com>
2025-06-19 13:08:52 -0300

    feat: adiciona seção ao menu de estatísticas rápidas

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,12 +4,12 @@ import Logo from './Logo'
 
 function Sidebar() {
     return (
-        <div className='w-[300px] min-h-screen bg-white fixed'>
+        <div className='w-[300px] min-h-screen hidden lg:block bg-white fixed'>
             <div className='p-6 border-b-[1px] border-gray-200'>
                 <Logo />
             </div>
             <nav className='p-6'>
-                <h2 className='uppercase mb-3 text-slate-400 font-semibold text-[12px] tracking-wider'>Menu Principal</h2>
+                <h2 className='uppercase mt-3 mb-3 text-slate-400 font-semibold text-[12px] tracking-wider'>Menu Principal</h2>
                 <div className='space-y-2'>
                     <SidebarItem icon={<Calculator size={20} />}>Criar Orçamento</SidebarItem>
                     <SidebarItem icon={<FileText size={20} />}>Visualizar Orçamentos</SidebarItem>
@@ -17,6 +17,20 @@ function Sidebar() {
                     <SidebarItem icon={<Plus size={20} />}>Criar Cliente</SidebarItem>
                 </div>
             </nav>
+            <div className='p-6'>
+                <h2 className='uppercase mb-3 text-slate-400 font-semibold text-[12px] tracking-wider'>Estatísticas Rápidas</h2>
+                <div className='space-y-3'>
+                    <div className='flex items-center justify-between text-[14px]'>
+                        <span className='text-gray-600'>Orçamentos Criados</span>
+                        <div className='inline-flex px-2.5 py-0.5 bg-gray-100 rounded-[14px] font-semibold'>10</div>
+                    </div>
+                    <div className='flex items-center justify-between text-[14px]'>
+                        <span className='text-gray-600'>Clientes Cadastrados</span>
+                        <div className='inline-flex px-2.5 py-0.5 bg-gray-100 rounded-[14px] font-semibold'>10</div>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     );
 }
