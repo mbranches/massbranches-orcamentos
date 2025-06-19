4655ea3252a3b21be2405bbeb74671a467ad8c49
mbranches <marcusvbranches@gmail.com>
2025-06-19 11:13:30 -0300

    refactor: altera logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,16 +2,16 @@ import { Building2 } from "lucide-react";
 
 function Logo() {
     return (
-        <div className='relative flex gap-2'>
-            <div className="p-3 rounded-lg bg-blue-600">
-                <Building2 size={32} color='white'/>
+        <div className='relative flex items-center gap-2'>
+            <div className="h-10 w-10 rounded-lg bg-blue-700 flex justify-center items-center">
+                <Building2 size={24} color='white'/>
             </div>
             
-            <div className='ml-2'>
+            <div className='ml-1'>
                 <h1 className='text-[20px] font-bold flex flex-col'>
                     Orçamentos
                 </h1>
-                <p className='text-slate-400'> Gestão de Orçamentos</p>
+                <p className='text-slate-500'> Gestão de Orçamentos</p>
             </div>
         </div>
     );
