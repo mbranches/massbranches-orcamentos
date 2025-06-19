31de166a425f55156bb7f2c491db13e3accc9413
mbranches <marcusvbranches@gmail.com>
2025-06-19 15:42:04 -0300

    feat: adiciona header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Calculator, FileText, MenuIcon, Plus, Users } from "lucide-react";
 import CardMenu from "./components/CardMenu";
 import Sidebar from './components/Sidebar'; 
 import { useState } from "react";
+import ShowUser from './components/ShowUser';
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -10,14 +11,20 @@ function App() {
     <div className="bg-slate-200">
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-      <div className="lg:hidden fixed top-5 left-5">
-        <button className="cursor-pointer" onClick={() => setSidebarOpen(!sidebarOpen)}>
-          <MenuIcon />
-        </button>
-      </div>
+      <header className="flex justify-between p-6 bg-white shadow-lg shadow-slate-300">
+        <div className="lg:hidden">
+          <button className="cursor-pointer" onClick={() => setSidebarOpen(!sidebarOpen)}>
+            <MenuIcon />
+          </button>
+        </div>
+
+        <div className="flex-1"></div>
+
+        <ShowUser />
+      </header>
       
 
-      <main className="min-h-screen flex justify-center items-center">
+      <main className="min-h-screen mt-[-15px] flex justify-center items-center">
 
         <div className="lg:ml-[310px] p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 max-w-6xl">
           <CardMenu icon={<Calculator size={24}/>} title="Criar Orçamento" description="Novo orçamento de obra" color="green"/>
