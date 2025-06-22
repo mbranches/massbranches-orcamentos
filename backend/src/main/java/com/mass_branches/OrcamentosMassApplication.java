af4d3dd48c32a505e24db9f1f5c5ba60dcc4de82
mbranches <marcusvbranches@gmail.com>
2025-06-22 11:24:15 -0300

    feat: adiciona projeto inicial do backend

---
+++ backend/src/main/java/com/mass_branches/OrcamentosMassApplication.java
@@ -0,0 +1,13 @@
+package com.mass_branches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentosMassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentosMassApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mass_branches/OrcamentosMassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mass_branches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentosMassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
