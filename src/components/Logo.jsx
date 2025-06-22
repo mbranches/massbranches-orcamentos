78898633ff5daad94aaf3ebfa1afbc9c61a0ceed
mbranches <marcusvbranches@gmail.com>
2025-06-22 11:17:05 -0300

    feat: adiciona redirecionamento à tela inicial ao clicar na logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,11 @@
 import { Building2 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Logo() {
+    const navigate = useNavigate();
+    
     return (
-        <div className='relative flex items-center gap-2'>
+        <div className='relative flex items-center gap-2 cursor-pointer' onClick={() => navigate("/home")}>
             <div className="h-10 w-10 rounded-lg bg-blue-700 flex justify-center items-center">
                 <Building2 size={24} color='white'/>
             </div>
