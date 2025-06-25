c8f17a19047ee95eeb27dfa33fb99e2cf7a1968d
mbranches <marcusvbranches@gmail.com>
2025-06-25 15:46:30 -0300

    feat: adiciona rota para página de login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import CreateBudget from './pages/CreateBudget.jsx'
+import Login from './pages/Login.jsx'
 
 const router = createBrowserRouter(
   [
+    {
+      path: "/login",
+      element: <Login />
+    },
     {
       path: "/home",
       element: <App />
