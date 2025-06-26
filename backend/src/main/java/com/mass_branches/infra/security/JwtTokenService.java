9ff064cc306f87302a08e261ec65b5d9434c4489
mbranches <marcusvbranches@gmail.com>
2025-06-26 11:15:01 -0300

    feat: adiciona claim com as roles do user no jwt

--- backend/src/main/java/com/mass_branches/infra/security/JwtTokenService.java
+++ backend/src/main/java/com/mass_branches/infra/security/JwtTokenService.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.mass_branches.model.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -23,6 +24,7 @@ public class JwtTokenService {
                 .withIssuedAt(creationDate())
                 .withExpiresAt(expirationDate())
                 .withSubject(user.getId())
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList())
                 .sign(algorithm);
     }
 
