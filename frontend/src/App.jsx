570b73e04fa18964918e6089d47836696e0636e2
mbranches <marcusvbranches@gmail.com>
2025-06-25 21:41:40 -0300

    remove: remove style desnecessária

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <div className="bg-slate-200">
       <PanelLayout sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} actualSection={"tela-inicial"} />
 
-      <main className="min-h-screen mt-[-15px] flex justify-center items-center">
+      <main className="min-h-screen flex justify-center items-center">
 
         <div className="lg:ml-[310px] mt-15 md:mt-0 p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 max-w-6xl ">
           <CardMenu icon={<Calculator size={24}/>} title="Criar Orçamento" description="Novo orçamento de obra" color="green" onButtonClick={() => navigate('/orcamentos/criar')}/>
