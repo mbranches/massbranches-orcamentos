c726dbb4f940c76e353df4f3e012493428102f77
mbranches <marcusvbranches@gmail.com>
2025-06-28 11:19:48 -0300

    chore: adiciona sql com inserts iniciais

---
+++ backend/src/main/resources/db/sql/inserts_default.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO role(idrole, name, description) VALUES(1, 'ADMIN', 'has all system permissions ');
+INSERT IGNORE INTO role(idrole, name, description) VALUES(2, 'CLIENT', 'has limited system permissions ');
+
+INSERT IGNORE INTO customer_type(idcustomer_type, name) VALUES(1, 'PESSOA_FISICA');
+INSERT IGNORE INTO customer_type(idcustomer_type, name) VALUES(1, 'PESSOA_JURIDICA');
