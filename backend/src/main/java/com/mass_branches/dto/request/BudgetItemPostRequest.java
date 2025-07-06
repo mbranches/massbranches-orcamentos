33735a0964d6b1281a53d9d24113eaa102e62825
mbranches <marcusvbranches@gmail.com>
2025-07-06 11:27:54 -0300

    feat: adiciona validação à dtos de request

--- backend/src/main/java/com/mass_branches/dto/request/BudgetItemPostRequest.java
+++ backend/src/main/java/com/mass_branches/dto/request/BudgetItemPostRequest.java
@@ -1,11 +1,18 @@
 package com.mass_branches.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 public record BudgetItemPostRequest(
+        @NotNull(message = "The field 'itemId' is required")
         Long itemId,
+        @NotBlank(message = "The field 'order' is required")
         String order,
+        @NotNull(message = "The field 'unitPrice' is required")
         BigDecimal unitPrice,
+        @NotNull(message = "The field 'quantity' is required")
         BigDecimal quantity
 ) {
 }
--- backend/src/main/java/com/mass_branches/dto/request/BudgetPostRequest.java
+++ backend/src/main/java/com/mass_branches/dto/request/BudgetPostRequest.java
@@ -1,10 +1,15 @@
 package com.mass_branches.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 public record BudgetPostRequest(
         String customerId,
+        @NotBlank(message = "The field 'description' is required")
         String description,
+        @NotBlank(message = "The field 'proposalNumber' is required")
         String proposalNumber,
         BigDecimal bdi
 ) {}
