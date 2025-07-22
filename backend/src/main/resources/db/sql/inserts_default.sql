24d7242bce65af31457108ccf4fcc1f4126d160e
mbranches <marcusvbranches@gmail.com>
2025-07-22 14:55:07 -0300

    refactor: altera id de customer type default

--- backend/src/main/resources/db/sql/inserts_default.sql
+++ backend/src/main/resources/db/sql/inserts_default.sql
@@ -2,4 +2,4 @@ INSERT IGNORE INTO role(idrole, name, description) VALUES(1, 'ADMIN', 'has all s
 INSERT IGNORE INTO role(idrole, name, description) VALUES(2, 'CLIENT', 'has limited system permissions ');
 
 INSERT IGNORE INTO customer_type(idcustomer_type, name) VALUES(1, 'PESSOA_FISICA');
-INSERT IGNORE INTO customer_type(idcustomer_type, name) VALUES(1, 'PESSOA_JURIDICA');
+INSERT IGNORE INTO customer_type(idcustomer_type, name) VALUES(2, 'PESSOA_JURIDICA');
