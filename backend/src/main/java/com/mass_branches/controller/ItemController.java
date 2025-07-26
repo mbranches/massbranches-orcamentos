4916909777c3366d150480e6fa9b00529991f742
mbranches <marcusvbranches@gmail.com>
2025-07-26 11:35:41 -0300

    fix: corrige url de listAllMy

--- backend/src/main/java/com/mass_branches/controller/ItemController.java
+++ backend/src/main/java/com/mass_branches/controller/ItemController.java
@@ -37,7 +37,7 @@ public class ItemController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping
+    @GetMapping("/me")
     public ResponseEntity<List<ItemGetResponse>> listAllMy(
             Authentication authentication,
             @RequestParam(required = false) Optional<String> name
