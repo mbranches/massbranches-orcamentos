ddfc2d3a8f3fe73c13b747ab4e171929f3badb94
mbranches <marcusvbranches@gmail.com>
2025-07-27 17:10:51 -0300

    remove: remove description do dto de post customer

--- backend/src/main/java/com/mass_branches/dto/request/CustomerPostRequest.java
+++ backend/src/main/java/com/mass_branches/dto/request/CustomerPostRequest.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 public record CustomerPostRequest(
         @NotBlank(message = "The field 'name' is required")
         String name,
-        String description,
         @NotBlank(message = "The field 'type' is required")
         String type
 ) {}
