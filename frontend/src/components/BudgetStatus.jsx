d475490519ce37677f513342d9d772b74ae50fca
mbranches <marcusvbranches@gmail.com>
2025-07-27 14:59:44 -0300

    feat: adiciona borda a  BudgetStatus

--- frontend/src/components/BudgetStatus.jsx
+++ frontend/src/components/BudgetStatus.jsx
@@ -1,9 +1,9 @@
 function BudgetStatus({ status }) {
     const mappedColor = {
-        "aprovado": "bg-green-100 text-green-800",
-        "recusado": "bg-red-100 text-red-800",
-        "em andamento": "bg-blue-100 text-blue-800",
-        "em análise": "bg-yellow-100 text-yellow-800",
+        "aprovado": "bg-green-100 text-green-800 border border-green-300",
+        "recusado": "bg-red-100 text-red-800 border border-red-300",
+        "em andamento": "bg-blue-100 text-blue-800 border border-blue-300",
+        "em análise": "bg-yellow-100 text-yellow-800 border border-yellow-300",
     };
 
     const formattedStatus = status ? status[0].toUpperCase() + status.slice(1).toLowerCase() : null;
