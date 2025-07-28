72fb26332ffd790dcccba230384dab5ab919537a
mbranches <marcusvbranches@gmail.com>
2025-07-28 17:17:45 -0300

    refactor: altera estilo dos cards da tela inicial

--- frontend/src/components/CardMenu.jsx
+++ frontend/src/components/CardMenu.jsx
@@ -14,16 +14,18 @@ const bgMap = {
 
 function CardMenu({title, description, icon, color, onButtonClick}) {
     return (
-        <div className="bg-white rounded-md shadow-lg flex flex-col space-y-1.5">
-            <div className="flex items-center p-6 pb-4 space-x-4 mr-20">
-                <div className={`w-[45px] h-[45px] ${bgMap[color]} flex items-center justify-center rounded-[14px] `}>
+        <div className="bg-white rounded-md shadow-lg flex flex-col justify-between space-y-1.5">
+            <div className="flex items-center w-full p-6 pb-4 space-x-4 mr-20">
+                <div className={`w-[45px] h-[45px] p-3 ${bgMap[color]} flex items-center justify-center rounded-[14px] `}>
                     {icon}
                 </div>
+
                 <div>
                     <h2 className="text-[18px] font-semibold">{title}</h2>
                     <p className="text-[14px] text-slate-600">{description}</p>
                 </div>
             </div>
+            
             <div className="p-6 pt-0"> 
                 <button className={`${buttonMap[color]} w-full px-8 py-2 rounded-md text-white font-semibold cursor-pointer text-[14px]`} onClick={onButtonClick}>Acessar</button>
             </div>
