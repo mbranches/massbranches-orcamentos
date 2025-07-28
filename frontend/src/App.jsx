4b95060b4910ea5dc81a4dbe075f89245d2df24f
mbranches <marcusvbranches@gmail.com>
2025-07-28 17:15:33 -0300

    remove: remove card de criar cliente

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,14 +12,12 @@ function App() {
   return (
     <div className="bg-gray-100">
       <PanelLayout sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} actualSection={"tela-inicial"}>
-        <div className="lg:ml-[310px] mt-15 md:mt-0 p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 max-w-6xl ">
+        <div className="lg:ml-[310px] mt-15 md:mt-0 p-6 grid grid-cols-1 md:grid-cols-3 gap-6 max-w-6xl ">
           <CardMenu icon={<Calculator size={24}/>} title="Criar Orçamento" description="Novo orçamento de obra" color="green" onButtonClick={() => navigate('/orcamentos/criar')}/>
 
           <CardMenu icon={<FileText size={24}/>} title="Visualizar Orçamentos" description="Gerenciar orçamentos existentes" color="blue" onButtonClick={() => navigate('/orcamentos')}/>
           
           <CardMenu icon={<Users size={24}/>} title="Visualizar Clientes" description="Novo orçamento de obra" color="purple" onButtonClick={() => navigate('/clientes')}/>
-          
-          <CardMenu icon={<Plus size={24}/>} title="Criar Cliente" description="Cadastrar novo cliente" color="orange" onButtonClick={() => navigate('/clientes/criar')}/>
         </div>
       </PanelLayout>
     </div>
