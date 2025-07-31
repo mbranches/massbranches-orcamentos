468a1184e9fa0b2a44cf18d290f467d6f7cb82f3
mbranches <marcusvbranches@gmail.com>
2025-07-31 18:22:00 -0300

    refactor: altera nome do atributo type

--- backend/src/main/java/com/mass_branches/dto/response/CustomerByCustomerRank.java
+++ backend/src/main/java/com/mass_branches/dto/response/CustomerByCustomerRank.java
@@ -1,8 +1,7 @@
 package com.mass_branches.dto.response;
 
-import com.mass_branches.model.Customer;
 import com.mass_branches.model.CustomerTypeName;
 
-public record CustomerByCustomerRank(String id, String name, CustomerTypeName typeName, Long numberOfBudgets) {
+public record CustomerByCustomerRank(String id, String name, CustomerTypeName type, Long numberOfBudgets) {
 
 }
