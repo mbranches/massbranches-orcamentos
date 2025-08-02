2d8e4eb8530194a342001c67b195c875d673e54e
mbranches <marcusvbranches@gmail.com>
2025-08-02 14:17:05 -0300

    docs: adiciona documentação para rota /api/v1/budgets/analytics

--- backend/src/main/java/com/mass_branches/controller/BudgetAnalyticsController.java
+++ backend/src/main/java/com/mass_branches/controller/BudgetAnalyticsController.java
@@ -4,6 +4,11 @@ import com.mass_branches.dto.response.ConversionRateByCustomerType;
 import com.mass_branches.dto.response.CustomerByCustomerRank;
 import com.mass_branches.model.User;
 import com.mass_branches.service.BudgetAnalyticsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +29,21 @@ import java.util.List;
 public class BudgetAnalyticsController {
     private final BudgetAnalyticsService service;
 
+    @Operation(
+            summary = "get conversion rate percentage of approved budgets of the requesting user",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "successfully returns conversion rate percentage of approved budgets of the requesting user",
+                            content = @Content(schema = @Schema(example = "33.33", type = "number"))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "the requesting user is not authenticated",
+                            content = @Content
+                    )
+            }
+    )
     @GetMapping("/conversion-rate")
     public ResponseEntity<BigDecimal> getBudgetConversionRate(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
@@ -33,6 +53,21 @@ public class BudgetAnalyticsController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(
+            summary = "get the budgeted total value with bdi of the requesting user",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "successfully returns the budgeted total value with bdi of the requesting user",
+                            content = @Content(schema = @Schema(example = "35789.59", type = "number"))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "the requesting user is not authenticated",
+                            content = @Content
+                    )
+            }
+    )
     @GetMapping("/total")
     public ResponseEntity<BigDecimal> getTotalBudgeted(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
@@ -42,6 +77,21 @@ public class BudgetAnalyticsController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(
+            summary = "get the approved total value with bdi of the requesting user",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "successfully returns the approved total value with bdi of the requesting user",
+                            content = @Content(schema = @Schema(example = "19887.00", type = "number"))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "the requesting user is not authenticated",
+                            content = @Content
+                    )
+            }
+    )
     @GetMapping("/total-approved")
     public ResponseEntity<BigDecimal> getTotalApproved(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
@@ -51,6 +101,34 @@ public class BudgetAnalyticsController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(
+            summary = "get conversion rate percentage of the requesting user by customer type",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "successfully returns conversion rate percentage of the requesting user by customer type",
+                            content = @Content(
+                                    schema = @Schema(example = """
+                                            [
+                                                {
+                                                    "type": "PESSOA_FISICA",
+                                                    "conversionRate": 68.6
+                                                },
+                                                {
+                                                    "type": "PESSOA_JURIDICA",
+                                                    "conversionRate": 47.5
+                                                }
+                                            ]
+                                    """)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "the requesting user is not authenticated",
+                            content = @Content
+                    )
+            }
+    )
     @GetMapping("/customers/type/conversion-rate")
     public ResponseEntity<List<ConversionRateByCustomerType>> getConversionRateByCustomerType(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
@@ -60,6 +138,25 @@ public class BudgetAnalyticsController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(
+            summary = "",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "get top 5 users with most budgets of the requesting user",
+                            content = @Content(
+                                    array = @ArraySchema(
+                                            schema = @Schema(implementation = CustomerByCustomerRank.class)
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "the requesting user is not authenticated",
+                            content = @Content
+                    )
+            }
+    )
     @GetMapping("/customers/top")
     public ResponseEntity<List<CustomerByCustomerRank>> getCustomerRank(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
--- backend/src/main/java/com/mass_branches/dto/response/CustomerByCustomerRank.java
+++ backend/src/main/java/com/mass_branches/dto/response/CustomerByCustomerRank.java
@@ -1,7 +1,15 @@
 package com.mass_branches.dto.response;
 
 import com.mass_branches.model.CustomerTypeName;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record CustomerByCustomerRank(String id, String name, CustomerTypeName type, Long numberOfBudgets) {
-
-}
+public record CustomerByCustomerRank(
+        @Schema(example = "id-uuid-1", description = "customer's id")
+        String id,
+        @Schema(example = "Gerdau", description = "customer's name")
+        String name,
+        @Schema(description = "customer's type")
+        CustomerTypeName type,
+        @Schema(example = "25", type = "number", description = "number of customer budgets")
+        Long numberOfBudgets
+) {}
