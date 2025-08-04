dd6ce48083b2c1456665f3ba4f8e46c41bfa8d63
mbranches <marcusvbranches@gmail.com>
2025-08-04 12:46:40 -0300

    docs: adiciona documentação para rota /api/v1/users

--- backend/src/main/java/com/mass_branches/controller/UserController.java
+++ backend/src/main/java/com/mass_branches/controller/UserController.java
@@ -3,6 +3,10 @@ package com.mass_branches.controller;
 import com.mass_branches.dto.response.UserGetResponse;
 import com.mass_branches.model.User;
 import com.mass_branches.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -20,8 +24,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService service;
 
+    @Operation(
+            summary = "Get the requesting user data",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "successfully get the requesting user data",
+                            content = @Content(schema = @Schema(implementation = UserGetResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "the requesting user is not authenticate",
+                            content = @Content
+                    )
+            }
+    )
     @GetMapping("/me")
-    public ResponseEntity<UserGetResponse> get(Authentication authentication) {
+    public ResponseEntity<UserGetResponse> getMe(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
 
         UserGetResponse response = service.getMe(user);
--- backend/src/main/java/com/mass_branches/dto/response/UserGetResponse.java
+++ backend/src/main/java/com/mass_branches/dto/response/UserGetResponse.java
@@ -1,8 +1,18 @@
 package com.mass_branches.dto.response;
 
 import com.mass_branches.model.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record UserGetResponse(String id, String firstName, String lastName, String email) {
+public record UserGetResponse(
+        @Schema(example = "user-uuid-1", description = "user id")
+        String id,
+        @Schema(example = "admin", description = "user first name")
+        String firstName,
+        @Schema(example = "mass", description = "user last name")
+        String lastName,
+        @Schema(example = "admin@admin.com", description = "user id")
+        String email
+) {
     public static UserGetResponse by(User user) {
         return new UserGetResponse(user.getId(), user.getFirstName(), user.getLastName(), user.getEmail());
     }
